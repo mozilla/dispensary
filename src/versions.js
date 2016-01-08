7597bca82846fec4489f35007056a724f28b201f
Matthew Riley MacPherson <matt@lonelyvegan.com>
2016-01-08 01:08:26 +0000

    Fix tests

--- src/versions.js
+++ src/versions.js
@@ -3,19 +3,22 @@ import request from 'request';
 import { uniqueArray } from 'utils';
 
 
-export function getVersions(repo, libraryInfo={}) {
+export function getVersions(repo, libraryInfo={}, _request=request) {
   if (!libraryInfo || !Object.keys(libraryInfo).length) {
-    return Promise.reject(`No library info supplied for ${repo}`);
+    return Promise.reject(new Error(`No library info supplied for "${repo}"`));
   }
 
   var versions = _getVersionsFromLibraries(repo, libraryInfo) || [];
 
   if (libraryInfo.useNPM) {
-    return _getVersionsFromNPM(repo, libraryInfo)
+    return _getVersionsFromNPM(repo, libraryInfo, _request)
       .then((versionsFromNPM) => {
         return uniqueArray(versions.concat(versionsFromNPM)).sort((a, b) => {
           return parseFloat(a) > parseFloat(b);
         });
+      })
+      .catch((err) => {
+        return Promise.reject(err);
       });
   }
 
@@ -29,7 +32,8 @@ function _getVersionsFromNPM(repo, libraryInfo, _request=request) {
       url: `https://registry.npmjs.org/${repo}`,
     }, (err, response, data) => {
       if (response.statusCode !== 200) {
-        return reject(new Error(`Repo ${repo} not found or an error occured.`));
+        return reject(
+          new Error(`node module "${repo}" not found or an error occured.`));
       }
 
       var versions = Object.keys(data.versions);
--- tests/test.versions.js
+++ tests/test.versions.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+
+import { DEFAULT_LIBRARY_FILE } from 'const';
 import { getVersions } from 'versions';
 
 import { unexpectedSuccess } from './helpers';
@@ -5,22 +8,31 @@ import { unexpectedSuccess } from './helpers';
 
 describe('Versions', function() {
 
+  var libraryInfo = JSON.parse(fs.readFileSync(DEFAULT_LIBRARY_FILE));
+
   it('should get many versions', () => {
     // HACK: We should use mock data to skip the need for this.
     this.timeout(50000);
 
-    return getVersions('angular')
+    return getVersions('angular', libraryInfo.angular)
       .then((versions) => {
-        assert.include(versions, '0.0.1');
-        assert.include(versions, '0.0.1-2');
-        assert.include(versions, '0.0.4');
-        assert.include(versions, '1.0.0');
+        assert.include(versions, '1.0.1');
+        assert.include(versions, '1.0.2');
         assert.include(versions, '1.4.8');
       });
   });
 
   it('should get versions for localForage', () => {
-    return getVersions('localforage')
+    var lfLibraryInfo = {
+      path: 'localforage.js',
+      pathToMinified: 'localforage.min.js',
+      useNPM: true,
+      // jscs:disable
+      url: 'https://raw.githubusercontent.com/mozilla/localForage/$VERSION/dist/$FILENAME', // eslint-disable-line
+      // jscs:enable
+    };
+
+    return getVersions('localforage', lfLibraryInfo)
       .then((versions) => {
         assert.include(versions, '1.3.0');
       });
@@ -32,22 +44,31 @@ describe('Versions', function() {
         throw new Error('Fail!');
       },
     };
-    return getVersions('localforage', fakeRequest)
+    return getVersions('angular', libraryInfo.angular, fakeRequest)
       .then(unexpectedSuccess)
       .catch((err) => {
         assert.instanceOf(err, Error);
         assert.equal(err.message, 'Fail!');
-
       });
   });
 
-  it('should handle 404s', () => {
-    return getVersions('does_not_exist')
+  it('should handle 404s on NPM', () => {
+    return getVersions('does_not_exist', {useNPM: true})
       .then(unexpectedSuccess)
       .catch((err) => {
         assert.instanceOf(err, Error);
         assert.equal(err.message,
-                     'Repo does_not_exist not found or an error occured.');
+          'node module "does_not_exist" not found or an error occured.');
+      });
+  });
+
+  it('should throw an error if no libraryInfo is present', () => {
+    return getVersions('angular')
+      .then(unexpectedSuccess)
+      .catch((err) => {
+        assert.instanceOf(err, Error);
+        assert.equal(err.message,
+                     'No library info supplied for "angular"');
       });
   });
 
